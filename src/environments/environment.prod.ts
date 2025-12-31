f181d01efc1c28cd74693f5e659af1b42782efc3
Markozrm <marcos.ramirez8727@alumnos.udg.mx>
2025-12-31 16:48:35 -0600

    Dummy_prod
    Dummy_prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,13 @@
 export const environment = {
   production: true,
   apiUrl: 'https://cheapserverhub.com',
+  PORT: '8443',
   apiUrl_apuestas: 'https://cheapserverhub.com',
-  PORT : '8443',
   apuesta_PORT: '3448',
-  quiniela_PORT: '3449',
   apiUrl_quiniela: 'https://cheapserverhub.com',
+  quiniela_PORT: '3449',
   apiUrl_ruleta: 'https://cheapserverhub.com',
+  ruleta_PORT: '3444',
+  apiUrl_chat: 'https://cheapserverhub.com',
+  chat_PORT: '3010'
 };
