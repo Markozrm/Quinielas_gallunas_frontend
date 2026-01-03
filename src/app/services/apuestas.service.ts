104617c991f958c5c0b3c77d9d5884097a09e8dc
Dayron <dayronpc24@gmail.com>
2026-01-03 11:51:02 -0600

    corregir la url

--- src/app/services/apuestas.service.ts
+++ src/app/services/apuestas.service.ts
@@ -9,8 +9,10 @@ import { environment } from 'src/environments/environment';
 })
 export class apuestaService {
   private httpClient = inject(HttpClient);
-  private apiUrl = environment.apiUrl_apuestas;
+  private apiUrl = environment.apiUrl;
+  private apiUrl_apuestas = environment.apiUrl_apuestas;
   private baseUrl: string;
+  private baseUrl_apuestas: string;
 
   private _users$ = new BehaviorSubject<UserType[]>([]);
   public users$ = this._users$.asObservable();
@@ -66,7 +68,7 @@ public ultimaRondaValida = this._ultimaRondaValida.asObservable();
     this.baseUrl = `${this.apiUrl}/api/apuestas`;
     
     // Inicializa el socket usando la librería nativa de socket.io-client
-    this.socket = io(`${this.apiUrl}`, {
+    this.socket = io(`${this.apiUrl_apuestas}`, {
       transports: ['websocket'],
     });
 
