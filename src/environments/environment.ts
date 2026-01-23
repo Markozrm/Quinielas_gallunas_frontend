842e245e22123ce30989f463af2e30bd8a84b020
Markozrm <marcos.ramirez8727@alumnos.udg.mx>
2026-01-23 15:42:37 -0600

    Contador
    Contador_apuestas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
   apiUrl_live: 'https://serverstream1.cheapserverhub.com',
   apiUrl_live_2: 'https://serverstream2.cheapserverhub.com',
   apiUrl_live_3: 'https://serverstream3.cheapserverhub.com',
-  apiUrl_apuestas: 'https://cheapserverhub.com',
+  apiUrl_apuestas: 'https://serverapuestas.cheapserverhub.com',
   apiUrl_quiniela: 'https://cheapserverhub.com',
   quiniela_PORT: '3449',
   apiUrl_ruleta: 'https://cheapserverhub.com',
